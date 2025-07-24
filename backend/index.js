bf22e0a95e03fd145a9d7733381fb3d2122cf204
mibinjoby <mibinjoby9@gmail.com>
2025-07-24 21:51:41 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.Mongo)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('server is running on port 3000 !!!');
-    
-})
+  console.log('Server is running on port 3000!!!');
+});

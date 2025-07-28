a59feeeb12016b172c75b29ff151ba145c2c2805
mibinjoby <mibinjoby9@gmail.com>
2025-07-28 17:25:20 +0530

    created signin  API route

--- backend/ApiRoutes/auth.route.js
+++ backend/ApiRoutes/auth.route.js
@@ -1,9 +1,10 @@
 
 import express from "express"
-import { signup } from "../Controllers/auth.controller.js";
+import { signin, signup } from "../Controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup)
+authRouter.post("/signin", signin)
 
 export default authRouter
--- backend/Controllers/auth.controller.js
+++ backend/Controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../Models/User.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/Error.js"
+import jwt  from "jsonwebtoken";
+
 
 export const signup = async (req, res , next) =>{
     
@@ -17,4 +19,26 @@ export const signup = async (req, res , next) =>{
     
    }
   };
+
+
+  export const signin = async (req, res, next) =>{
+    const {email, password} = req.body;
+    try {
+      const validUser = await User.findOne({email})
+
+      if(!validUser)return next(errorHandler(404,'❌ USER NOT FOUND'))  
+
+        const validpassword = bcryptjs.compareSync(password,validUser.password)
+
+      if (!validUser) return next (errorHandler(401,'wrong credentials ❌ '))
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const {password: pass, ... rest} = validUser._doc
+        res.cookie('access_token',token,{httpOnly: true }).status(200).json(validUser)
+      
+    } catch (error) {
+      next(error)
+      
+    }
+  }
   
